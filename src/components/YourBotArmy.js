0e13448159ee4bd3683511c3b37d49d024956cd7
sialotobiko <sialo.tobiko@student.moringaschool.com>
2022-10-04 16:28:24 +0300

    push project to this repository

---
+++ src/components/YourBotArmy.js
@@ -0,0 +1,24 @@
+import React from "react";
+import BotCard from "./BotCard";
+
+function YourBotArmy({botArmy, removeBot, deleteBot}) {
+  //your bot army code here...
+
+
+
+  console.log(botArmy)
+  return (
+    <div className="ui segment inverted olive bot-army">
+      <div className="ui five column grid">
+        <div className="row bot-army-row">
+          {botArmy.map((bot) => (
+            <BotCard bot={bot} handleBot={removeBot} handleDeleteBot={deleteBot}/>
+          ))}
+          Your Bot Army
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default YourBotArmy;
